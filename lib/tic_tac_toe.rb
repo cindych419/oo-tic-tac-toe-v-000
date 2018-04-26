53c365111aa42b8ca763ec17d75073d489287d83
cindych419@gmail.com <cindych419@gmail.com>
2018-04-26 19:25:45 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -79,4 +79,22 @@ def over?
   draw? || won? && full? || won? && !full?
 end
 
+def winner
+  if winning_combo = won?(
+    @board[winning_combo.first]
+  end
 end
+
+def play
+  while !over?
+    turn
+  end
+end
+
+if won?
+  puts "Congratulations #{winner}!"
+elsif draw?
+  puts "Cat's Game!"
+  end
+end
+
