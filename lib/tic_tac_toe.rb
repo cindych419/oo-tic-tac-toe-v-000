d9fc2a1da444d04e474ea301ba47ac9af33680bb
cindych419@gmail.com <cindych419@gmail.com>
2018-04-26 19:14:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,8 @@ class TicTacToe
   def initialize
     @board = Array.new(9, " ")
   end
-  WIN_COMBINATIONS = [
+
+WIN_COMBINATIONS = [
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -12,6 +13,7 @@ class TicTacToe
     [2,4,6],
     [0,4,8]
   ]
+
 def display_board
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   puts "-----------"
@@ -19,22 +21,27 @@ def display_board
   puts "-----------"
   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
+
 def input_to_index(user_input)
   user_input.to_i - 1
 end
+
 def move(index, current_player = "X")
   @board[index] = current_player
 end
+
 def position_taken?(index)
   @board[index] != " "
 end
+
 def valid_move?(index)
   if index.between?(0,8) && !position_taken?(index)
       true
   else
       false
-    end
   end
+end
+
 def turn
   puts "Please enter 1-9:"
   user_input = gets.strip
@@ -44,8 +51,17 @@ def turn
     else
         turn
     end
-    display_board
-    end
+  display_board
+end
+
+def turn_count
+  @board.count{|token| token == "X" || token == "O"}
+end
+
+def current_player
+turn_count % 2 == 0 ? "X" : "O"
+end
+
 def won?
   WIN_COMBINATIONS.detect do |combo|
   @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]] && position_taken?(combo[0])
@@ -55,24 +71,27 @@ end
 def full?
   @board.all?{|combo| combo == "X" || combo== "O"}
 end
+
 def draw?
-  !won?(@board) && full?(@board)
+  !won? && full?
 end
+
 def over?
-  draw?(@board) || won?(@board) && full?(@board) || won?(@board) && !full?(@board)
+  draw? || won? && full? || won? && !full?
 end
 def winner
   if winning_combo = won?(
     @board[winning_combo.first]
   end
 end
+
 def play
   while !over?
     turn
   end
 
   if won?
-    puts "Congratulations #{winner(board)}!"
+    puts "Congratulations #{winner}!"
   elsif draw?
     puts "Cat's Game!"
   end
