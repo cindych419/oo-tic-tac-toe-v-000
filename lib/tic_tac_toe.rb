2a2cda7c46769d1b0b5362ad241b9db234b7d351
cindych419@gmail.com <cindych419@gmail.com>
2018-04-26 18:32:21 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,4 +12,18 @@ class TicTacToe
     [2,4,6],
     [0,4,8]
   ]
+def display_board(board)
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
 end
